b32d123f8ad12a9aec1adb439ddb03f0183ca23b
SeobinYun <o0oseobin@naver.com>
2021-06-01 15:54:32 +0900

    add main()

--- Homework4/src/QuickSorting.java
+++ Homework4/src/QuickSorting.java
@@ -1,2 +1,15 @@
 public class QuickSorting {
+    public static void main(String[] args){
+        int[] array = new int[32];
+        for(int i=0; i<array.length; i++){
+            array[i] = (int) (Math.random()*31);
+            for(int j=0; j<i; j++){ // 중복값 확인: 배열 index j부터, j가 i전일 때까지 비교 반복
+                if(array[j]==array[i]){ // index j방의 값과 i방의 값이 값다면
+                    i--; // index i 위치의 값을 다시 할당받아야 하기 때문에 i--를 해주고 비교반복문 탈출
+                    break;
+                }
+                else{;}
+            }
+        }
+    }
 }
