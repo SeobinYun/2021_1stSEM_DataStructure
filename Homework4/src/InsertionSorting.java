04ae3197d77bdcb527facf71b94446980589644c
SeobinYun <o0oseobin@naver.com>
2021-06-02 16:54:27 +0900

    complete InsertionSorting

--- Homework4/src/InsertionSorting.java
+++ Homework4/src/InsertionSorting.java
@@ -11,16 +11,35 @@ public class InsertionSorting {
             }
         }
         System.out.print("배열생성 : ");
-        for(int i=0; i<array.length; i++) {
+        for(int i=0; i<array.length; i++) { // 랜덤으로 생성한 배열 출력
+            System.out.print(array[i] + "\t");
+        }
+
+        InsertionSorting(array); // InsertionSorting method 실행
+
+        System.out.print("\n최종배열출력 : ");
+        for(int i=0; i<array.length; i++){ // InsertionSorting method 실행 후 최종배열 출력
             System.out.print(array[i] + "\t");
         }
         System.out.println();
-
     }
 
-    private int[] InsertionSorting(int[] array){
-        int[] answer = new int[32];
+    private static void InsertionSorting(int[] array){ // InsertionSorting Method
+        int index = 1; // 몇단계인지 알려줄 index 변수
+        for(int i=0; i< array.length; i++){
+            int target = array[i]; // array[i]를 기준으로 삼음
+            int j=i-1; // 비교할 대상
+            while(j>=0 && target<array[j]){ // array[j]에 있는 값이 target보다 크다면 array[j]값을 array[j+1]로 밀어줌
+                array[j+1]=array[j];
+                j--;
+            }
+            array[j+1] = target; // 그리고 array[j+1]을 target으로 설정해줌
 
-        return answer;
+            System.out.print("\n"+index +"단계 : "); // 각 단계마다 배열의 상태를 확인해줌
+            for(int k=0; k<array.length; k++) {
+                System.out.print(array[k]+"\t");
+            }
+            index++;
+        }
     }
 }
